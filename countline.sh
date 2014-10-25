3e10df71582fac8971b3a37ad1c42aa4c9338d72
swiftcore <wanghit2006@163.com>
2014-10-25 19:23:57 +0800

    Update countline.sh

--- countline.sh
+++ countline.sh
@@ -1,3 +1,3 @@
-cat hello.py clnt.py server.py  main.c resql_funcs.c resql.h resql.l resql.y | wc -l
+cat showcase.py clnt.py server.py  main.c resql_funcs.c resql.h resql.l resql.y | wc -l
 
 #cat call_resql.py hello.py clnt.py serv.py extract.py main.c tests.c resql_funcs.c resql.tab.c resql.tab.h lex.yy.c tests.c | wc -l
